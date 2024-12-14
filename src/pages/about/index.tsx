be973c8513a8e97b417fc64bf62ae88e70adcee9
Fleege <76089935+pfleege@users.noreply.github.com>
2024-12-14 11:13:27 +0200

    ADD a pages folder with about- and customers pages

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+        excepturi, officia laborum perspiciatis beatae, rem inventore error fuga
+        omnis ipsa voluptas suscipit quo iure voluptatum esse explicabo ipsum
+        eum dolor, dignissimos nesciunt animi consequuntur. Suscipit
+        exercitationem veniam itaque inventore, ea, accusantium molestiae ipsum
+        iusto aliquid totam, officia vitae tenetur dolorem?
+      </p>
+    </div>
+  );
+};
+
+export default About;
