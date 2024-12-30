578a50b8345751e625674af54ab02fd1818014fa
Fleege <76089935+pfleege@users.noreply.github.com>
2024-12-30 10:39:15 +0200

    ADD content to customers page

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -45,7 +45,21 @@ const Customers = () => {
 
   return (
     <div className="flex flex-col relative top-0 left-0 w-full h-screen z-30 font-specialElite">
-      <div className="mt-[450px] flex justify-center w-full bg-opacity-40 bg-stone-700 z-20">
+      <section className="mt-[450px] flex m-auto z-20 w-full">
+        <div className=" h-[250px] w-full flex flex-col justify-center gap-2">
+          <h2 className="pl-[30vw] sm:pl-[33vw] md:pl-[45vw] xl:pl-[50vw] text-black text-7xl">
+            Mobile
+          </h2>
+          <h2 className="pl-[25vw] sm:pl-[27vw] md:pl-[35vw] xl:pl-[45vw] text-black text-4xl">
+            Or
+          </h2>
+          <h2 className="pl-[17vw] sm:pl-[20vw] md:pl-[30vw] xl:pl-[37vw] text-black text-6xl">
+            Web-Based
+          </h2>
+        </div>
+      </section>
+
+      <div className="mt-[50px] flex justify-center w-full bg-opacity-40 bg-stone-700 z-20">
         {/* <div className="w-full max-w-screen-2xl"> */}
         <div className="pt-[140px] flex justify-center h-[1080px] max-w-[520px] max-h-[1080px] bg-[url('/smartPhoneFilled.png')] bg-center bg-no-repeat">
           <div className="grid grid-cols-[repeat(auto-fit,min(520px))] max-h-[800px] place-content-center gap-4 scrollbar-hide overflow-y-scroll">
@@ -67,6 +81,34 @@ const Customers = () => {
                 <p className="ml-2 text-2xl text-white">
                   City:
                   <span className="p-2 inline text-xl text-black">
+                    {customer.address.city}
+                  </span>
+                </p>
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        <div className="m-10 pt-[288px] flex justify-center h-[1080px] max-w-[520px] max-h-[1080px] bg-[url('/webBased.jpg')] bg-center bg-no-repeat border-2 border-yellow-50 rounded-2xl">
+          <div className="grid grid-cols-[repeat(3,min(170px))] max-h-[530px] place-content-start gap-1 scrollbar-hide overflow-y-scroll">
+            {customers.map((customer) => (
+              <Link
+                href={`/customers/${customer.id}`}
+                className="flex flex-col w-1/4 min-w-[170px] h-40 p-2 pt-4 bg-[url('/contactBg.png')] bg-cover bg-center"
+                key={customer.id}
+              >
+                <h2 className="text-sm pt-2 font-bold border-l-8 border-amber-400 text-white">
+                  {customer.name}
+                </h2>
+                <p className="mt-5 text-[8px] text-white">
+                  Company:
+                  <span className="p-2 inline text-[8px] text-black">
+                    {customer.company.name}
+                  </span>
+                </p>
+                <p className="text-[8px] text-white">
+                  City:
+                  <span className="p-2 inline text-[8px] text-black">
                     {customer.address.city}
                   </span>
                 </p>
